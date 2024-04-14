847bcf09d6d2b60de8e03de80c32a6d03b0d8adc
Nyx Iskandar <xyntechx@gmail.com>
2024-04-14 09:29:31 -0700

    Basic /account with profiles data + refactoring (#4)
    * 🔨 Refactor code
    
    * 🔨 Refactor code
    
    * Implement basic /account with profiles data

--- src/app/reviews/thank-you/page.tsx
+++ src/app/reviews/thank-you/page.tsx
@@ -1,10 +1,12 @@
-import Link from 'next/link';
+import Link from "next/link";
 
 const ThankYouPage = () => {
     return (
         <>
-            <h1 className="text-4xl font-bold p-4">Thank You!</h1>
-            <p className="text-l font-bold p-4">Your review has been submitted successfully.</p>
+            <h1 className="p-4 text-4xl font-bold">Thank You!</h1>
+            <p className="text-l p-4 font-bold">
+                Your review has been submitted successfully.
+            </p>
             <Link
                 href="/reviews"
                 className="flex items-center justify-center rounded-full border-2 border-primary-blue bg-primary-blue px-4 py-1 text-white transition-shadow hover:shadow-lg"

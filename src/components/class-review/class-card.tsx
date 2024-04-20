49356718cb832b57006496d421b11992f7034430
xyntechx <xyntechx@gmail.com>
2024-04-19 22:57:10 -0700

    Include optional linkComponent arg

--- src/components/class-review/class-card.tsx
+++ src/components/class-review/class-card.tsx
@@ -1,4 +1,5 @@
-import RatingBox from '@/components/class-review/rating-box';
+import RatingBox from "@/components/class-review/rating-box";
+import { ReactNode } from "react";
 
 interface Rating {
     name: string;
@@ -7,26 +8,39 @@ interface Rating {
 
 interface ClassCardProps {
     id: string;
-    className: string;
+    name: string;
     ratings: Rating[];
     overview: string;
+    linkComponent?: ReactNode;
 }
 
-const ClassCard = ({ id, className, ratings, overview }: ClassCardProps) => {
+const ClassCard = ({
+    id,
+    name,
+    ratings,
+    overview,
+    linkComponent,
+}: ClassCardProps) => {
     return (
-        <div className="bg-white p-4 rounded-lg border border-gray-200 shadow-lg max-w-3xl">
-            <h2 className="text-3xl font-bold text-center p-6">{className}</h2>
+        <div className="flex w-full flex-col items-center justify-center gap-y-8 rounded-lg border border-gray-200 bg-white p-10 shadow-lg">
+            <h2 className="text-center text-3xl font-bold">{name}</h2>
             <div className="flex flex-wrap justify-center gap-8">
                 {ratings.map((rating) => (
                     <RatingBox
-                        key={`${id}-${rating.name}`} 
+                        key={`${id}-${rating.name}`}
                         ratingName={rating.name}
                         ratingValue={rating.value}
                     />
                 ))}
             </div>
-            <h3 className="text-lg uppercase font-bold text-left p-8">Class Overview:</h3>
-            <p className=" pl-8 pb-8">{overview}</p>
+            <div className="flex w-full flex-col items-center justify-center">
+                <h3 className="w-full text-left text-lg font-bold uppercase">
+                    Class Overview
+                </h3>
+                <p className="w-full text-left">{overview}</p>
+            </div>
+
+            {linkComponent}
         </div>
     );
 };

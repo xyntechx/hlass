285f6d280dd85dcbb26007f227e7ef7d875dacc8
xyntechx <xyntechx@gmail.com>
2024-04-19 23:48:52 -0700

    Update linkComponent position

--- src/components/class-review/class-card.tsx
+++ src/components/class-review/class-card.tsx
@@ -22,7 +22,10 @@ const ClassCard = ({
     linkComponent,
 }: ClassCardProps) => {
     return (
-        <div className="flex w-full flex-col items-center justify-center gap-y-8 rounded-lg border border-gray-200 bg-white p-10 shadow-lg">
+        <div className="relative flex w-full flex-col items-center justify-center gap-y-8 rounded-lg border border-gray-200 bg-white p-10 shadow-lg">
+            <div className="absolute right-[20px] top-[20px]">
+                {linkComponent}
+            </div>
             <h2 className="text-center text-3xl font-bold">{name}</h2>
             <div className="flex flex-wrap justify-center gap-8">
                 {ratings.map((rating) => (
@@ -39,8 +42,6 @@ const ClassCard = ({
                 </h3>
                 <p className="w-full text-left">{overview}</p>
             </div>
-
-            {linkComponent}
         </div>
     );
 };

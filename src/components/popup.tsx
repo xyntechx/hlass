74060dc8d90ae8bb69fc92cb2fc43f2a7871982e
Ryan Schwartz <ryanschwartz@berkeley.edu>
2024-05-16 15:55:09 -0700

    Removed testing code

--- src/components/popup.tsx
+++ src/components/popup.tsx
@@ -16,7 +16,7 @@ const Popup = ({
     onViewClass,
     children 
 } : PopupProps) => {
-    /* useEffect(() => {
+    useEffect(() => {
         if (show) {
             document.body.style.overflow = 'hidden';
         } else {
@@ -25,7 +25,7 @@ const Popup = ({
         return () => {
             document.body.style.overflow = 'auto';
         };
-      }, [show]); */
+      }, [show]);
     
       if (!show) return null;
     
@@ -49,18 +49,6 @@ const Popup = ({
                     </div>
                 </div>
                 {children}
-                <div>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam efficitur, nisi at vulputate varius, nisi nulla cursus nisi, non consectetur libero justo ut libero. Suspendisse potenti. Donec a est non urna volutpat consequat. Curabitur in lectus eu sapien tempor malesuada.</p>
-                    <p>Phasellus vestibulum massa eget lacus interdum fermentum. Integer nec felis sit amet elit viverra vehicula vel a erat. Nam fermentum, augue id scelerisque consectetur, sapien nisi tincidunt nunc, non pharetra neque urna quis felis. Vivamus finibus, nunc eget gravida efficitur, leo nisi interdum ex, a posuere urna augue ut purus.</p>
-                    <p>Aliquam erat volutpat. Nulla facilisi. Maecenas consequat diam a nisi eleifend, quis feugiat nisl laoreet. Donec suscipit, libero ac bibendum facilisis, quam velit laoreet turpis, ac malesuada velit sapien at metus. Quisque id sagittis nisl. Praesent nec lectus sed mi iaculis posuere ac id elit.</p>
-                    <p>Mauris euismod metus in purus fringilla, sit amet aliquet mauris sagittis. Suspendisse potenti. Proin vehicula nulla vel urna vehicula, ut ultricies felis tincidunt. Nam vehicula ipsum eget turpis dictum, ac tincidunt turpis fermentum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Integer et nunc id dui tincidunt vestibulum et ut elit.</p>
-                    <p>Curabitur condimentum, lectus et auctor posuere, risus ipsum facilisis justo, eget fermentum justo est in eros. Curabitur id lacus non elit euismod vehicula. In in ultricies arcu. Nullam ac varius lacus, ut tristique elit. Etiam sodales, metus et sollicitudin gravida, eros lorem sagittis risus, sed fermentum mi magna a elit.</p>
-                    <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Etiam a libero et turpis tristique pretium. Integer a nisi sit amet justo egestas tincidunt a sit amet nulla. Proin sit amet dapibus tortor, vel fermentum risus. Vivamus laoreet sapien a ligula suscipit, sit amet egestas lacus venenatis. Proin sit amet libero turpis. Integer in dui velit. Donec tempor velit ac ipsum suscipit, non interdum ex facilisis.</p>
-                    <p>Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; In ut leo nec ligula interdum fermentum nec id libero. Ut sollicitudin mi eget bibendum ullamcorper. Maecenas vel dolor arcu. Nullam vehicula vehicula odio. Duis quis nunc a justo dignissim faucibus in a odio. Etiam egestas justo vel elit ultricies, id ultrices odio facilisis. Nulla facilisi. Curabitur gravida turpis a varius hendrerit.</p>
-                    <p>Vivamus scelerisque risus vitae ligula bibendum, eget cursus nisl vehicula. Sed auctor ipsum sed bibendum vulputate. Nunc ut sem nec turpis iaculis cursus. Aenean commodo orci ac vehicula fermentum. Fusce interdum dui eu dolor lacinia fermentum. Integer sodales dui nisi, id elementum mi tincidunt eget.</p>
-                    <p>Morbi eget augue sed sapien scelerisque dictum. Nulla facilisi. Nulla sodales lorem et libero suscipit, vitae viverra mauris congue. Phasellus dictum vel tortor eget tincidunt. Ut et lectus eu augue tincidunt eleifend. Nulla facilisi. Fusce non felis sit amet erat pulvinar cursus in non sem.</p>
-                    <p>Curabitur et tortor non purus vehicula condimentum. Etiam at lacus quis nunc pulvinar varius. Vivamus eget metus et purus aliquam laoreet. Ut vitae malesuada nisl. Ut lacinia augue id libero suscipit, quis hendrerit lorem vulputate. Fusce eget magna in dui aliquet cursus.</p>
-                </div>
             </div>
         </div>
       );

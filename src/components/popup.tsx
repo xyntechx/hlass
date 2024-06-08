6d5ccc07ffdddf8f3f4123abcec9e79ac461f00f
xyntechx <xyntechx@gmail.com>
2024-06-08 09:58:14 +0800

    Close popup onClick of region outside of it

--- src/components/popup.tsx
+++ src/components/popup.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import React, { ReactNode, useEffect } from 'react';
+import React, { ReactNode, useEffect, useRef } from "react";
+import useDetectOutsideClick from "@/lib/hooks/useDetectOutsideClick";
 import Image from "next/image";
 
 interface PopupProps {
@@ -10,36 +11,41 @@ interface PopupProps {
     children: ReactNode;
 }
 
-const Popup = ({ 
-    show, 
-    onClose,
-    onViewClass,
-    children 
-} : PopupProps) => {
+const Popup = ({ show, onClose, onViewClass, children }: PopupProps) => {
+    const wrapperRef = useRef(null);
+    useDetectOutsideClick(wrapperRef, onClose);
+
     useEffect(() => {
         if (show) {
-            document.body.style.overflow = 'hidden';
+            document.body.style.overflow = "hidden";
         } else {
-            document.body.style.overflow = 'auto';
+            document.body.style.overflow = "auto";
         }
         return () => {
-            document.body.style.overflow = 'auto';
+            document.body.style.overflow = "auto";
         };
-      }, [show]);
-    
-      if (!show) return null;
-    
-      return (
-        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-            <div className="relative bg-white p-6 rounded-lg shadow-lg w-3/4 h-3/4 overflow-y-auto">
+    }, [show]);
+
+    if (!show) return null;
+
+    return (
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+            <div
+                ref={wrapperRef}
+                className="relative h-3/4 w-3/4 overflow-y-auto rounded-lg bg-white p-6 shadow-lg"
+            >
                 <div className="flex flex-row justify-between pb-4">
-                    <button className="text-3xl hover:text-gray-800" onClick={onClose}>
+                    <button
+                        className="text-3xl hover:text-gray-800"
+                        onClick={onClose}
+                    >
                         &times;
                     </button>
-                    <div onClick={onViewClass} className="flex flex-row font-semibold text-black hover:text-gray-800 gap-x-1">
-                        <button>
-                            OPEN FULL PAGE VIEW
-                        </button>
+                    <div
+                        onClick={onViewClass}
+                        className="flex flex-row gap-x-1 font-semibold text-black hover:text-gray-800"
+                    >
+                        <button>OPEN FULL PAGE VIEW</button>
                         <Image
                             src="/full_page_arrow.svg"
                             alt="Open Full Page View"
@@ -51,7 +57,7 @@ const Popup = ({
                 {children}
             </div>
         </div>
-      );
+    );
 };
 
 export default Popup;
